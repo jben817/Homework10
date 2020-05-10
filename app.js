c5cbfa827d7dd6bc025cb6d902d73c808f0536cd
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-05-10 03:27:41 -0400

    added but still doesn't work

--- app.js
+++ app.js
@@ -14,25 +14,6 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-class Employee {
-    constructor(name, id, email){
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    }
-    getName(){
-        console.log
-    }
-    getID(){
-
-    }
-    getEmail(){
-
-    }
-    getRole(){
-        
-    }
-}
 ​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
